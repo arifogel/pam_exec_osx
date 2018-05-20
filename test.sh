730936ec94bc9362a2221ae52346957349b81355
Ari Fogel <ari@fogelti.me>
2018-05-19 17:08:22 -0700

    more verbose test failure

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@ set -e
 ./configure CFLAGS='-Wall -Wextra -Werror -Wno-unused-parameter -O0 -g'
 make clean
 make
-make check
+if ! make check; then
+  echo "Test failure!" 1>&2
+  find . -name '*.log' | sort | while read i; do echo "$i:"; cat "$i"; echo; done 1>&2
+  exit 1
+fi
 echo "Success! All tests passed."
 
